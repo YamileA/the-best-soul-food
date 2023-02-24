e7e708b9170f9e2d4f07a5c78e27dcda72f43a3f
YamileA <cp.yamileag@gmail.com>
2023-02-24 12:08:47 -0500

    :sparkles: Initial comit

---
+++ index.js
@@ -0,0 +1,41 @@
+const openModal = document.getElementById("open-modal");
+const send = document.getElementById("send");
+const modal = document.getElementById("modal");
+const closeModal = document.getElementById("close-modal")
+const formulary = document.getElementById("form")
+const modal2 = document.getElementById("modal2");
+
+//abrir modal
+openModal.addEventListener("click", ()=>{
+  modal.showModal();
+});
+
+//cerrar modal
+closeModal.addEventListener("click", ()=>{
+  modal.close();
+  formulary.reset()
+})
+
+//form -- fecha actual en adelante
+date.min = new Date().toISOString().split("T")[0];
+
+//envia y guarda los datos del formulario en un objeto
+function seend(){
+  formulary.addEventListener("submit", (e)=>{
+  e.preventDefault()
+  const data = Object.fromEntries(
+    new FormData(e.target)
+  )
+  //muestra el objeto
+  console.log(JSON.stringify(data))
+  //borrar campos del formulario
+  formulary.reset()
+  modal.close();
+  console.log("1")
+})
+}
+// abrir y cerrar modal2
+formulary.addEventListener("submit", ()=>{
+  setTimeout(function openModal2(){modal2.showModal()}, 500)
+  setTimeout(function closeModal2(){modal2.close()}, 4000)
+})
